7fab583e240250106f827cd30e610e76e2542ad2
Amanda Mok <a.mok@berkeley.edu>
2022-07-14 18:31:20 -0700

    update wrapper function

--- R/choros.R
+++ R/choros.R
@@ -1,31 +1,49 @@
 choros <- function(bam_fname, transcript_fa_fname,
                    transcript_length_fname, offsets_fname,
-                   num_genes=200, min_prop=0.9, f5_length=2, f3_length=3,
-                   model=formula(count ~ transcript + A + P + E + d5*genome_f5 + d3*genome_f3),
-                   expt_dir=".") {
+                   expt_dir=".", save_intermediate=T, extra_args=NULL) {
   # wrapper function to run choros pipeline
   ## bam_fname: character; file.path to .bam alignment file
   ## transcript_fa_fname: character; file path to transcriptome .fa file
   ## transcript_length_fname: character; file path to transcriptome lengths file
   ## offsets_fname: character; file.path to offset / A site assignment rules .txt file
-  ## min_prop: numeric; proportion of footprints to be modeled by d5/d3 lengths
-  ## f5_length: integer; length of 5' bias region
-  ## f3_length: integer; length of 3' bias region
-  ## num_genes: integer; number of genes used in regression model
+  ## expt_dir: character; file.path to directory to save output/intermediate files
+  ## save_intermediate: logical; whether to save intermediate files
+  ## extra_args : named list; additional parameters to pass onto internal functions
+  ### if False: only save bam file with corrected weights
   ## TODO: test for appropriate inputs
   ## TODO: flesh out function default settings
+  ## 0. parse extra_args
+  default_args <- list(num_genes=250, min_prop=0.9, f5_length=3, f3_length=3,
+                       model=formula(count ~ transcript + A + P + E + d5*f5 + d3*f3 + gc),
+                       compute_gc=T, gc_omit="APE", num_cores=NULL, load_bam_full=F,
+                       d5_d3_plot_title="", diagnostic_plot_title="")
+  if(!is.null(extra_args)) {
+    extra_args <- default_args
+    else {
+      not_specified <- names(default_args)[!(names(default_args) %in% names(extr_args))]
+      for(x in not_specified) {
+        extra_args[[x]] <- default_args[[x]]
+      }
+    }
+  }
+  # 1. generate diagnostic plot
   print("Creating diagnostic plot")
   transcript_lengths <- load_lengths(transcript_length_fname)
-  diagnostic_plot <- plot_diagnostic(bam_fname, transcript_length_fname)
-  save(diagnostic_plot, file=file.path(expt_dir, "diagnostic_plot.Rda"))
+  diagnostic_plot <- plot_diagnostic(bam_fname, transcript_length_fname,
+                                     plot_title=default_args$diagnostic_plot_title)
   # 1. read in footprint alignments
   print("Loading alignment file")
-  alignment_data <- load_bam(bam_fname, transcript_fa_fname, transcript_length_fname,
-                             offsets_fname, f5_length, f3_length)
-  save(alignment_data, file=file.path(expt_dir, "alignment.Rda"))
+  alignment_data <- load_bam(bam_fname, transcript_fa_fname,
+                             transcript_length_fname, offsets_fname,
+                             f5_length=default_args$f5_length,
+                             f3_length=default_args$f3_length,
+                             compute_gc=default_args$compute_gc,
+                             gc_omit=default_args$gc_omit,
+                             num_cores=default_args$num_cores,
+                             full=default_args$load_bam_full)
   # 2. compute size/frame subsets
   print("Computing d5/d3 subsets")
-  d5_d3 <- count_d5_d3(alignment_data)
+  d5_d3 <- count_d5_d3(alignment_data, plot_title=default_args$d5_d3_plot_title)
   d5_d3_subsets <- d5_d3$counts[1:(which(d5_d3$counts$proportion>min_prop)[1]),
                                 c("d5", "d3")]
   subset_names <- sapply(seq(nrow(d5_d3_subsets)),
@@ -33,16 +51,14 @@ choros <- function(bam_fname, transcript_fa_fname,
                            paste("d5", d5_d3_subsets$d5[x], "d3",
                                  d5_d3_subsets$d3[x], sep="_")
                          })
-  save(d5_d3, d5_d3_subsets, subset_names, file=file.path(expt_dir, "d5_d3_subsets.Rda"))
   # 3. choose training set: highest RPF density (RPF count / # aa)
   # TODO: filter for d5/d3 subsets before calculating
   # TODO: filter for transcripts that are too short (cds_length <= exclude_codons5 + exclude_codons3 + ???)
   print("Choosing transcripts for training")
-  transcript_counts <- calculate_transcript_density(alignment_data, transcript_length_fname,
+  transcript_counts <- calculate_transcript_density(alignment_data,
+                                                    transcript_length_fname,
                                                     statistic=mean)
   training_set <- names(transcript_counts[1:num_genes])
-  writeLines(training_set, file.path(expt_dir, "training_transcripts.txt"))
-  save(transcript_counts, file=file.path(expt_dir, "transcript_counts"))
   # 4. initialize data frame for regression
   print("Initializing regression data")
   training_data <- init_data(transcript_fa_fname, transcript_length_fname,
@@ -50,59 +66,67 @@ choros <- function(bam_fname, transcript_fa_fname,
                              f3_length=f3_length, which_transcripts=training_set)
   training_data$transcript <- relevel(training_data$transcript, ref=training_set[1])
   training_data$count <- count_footprints(alignment_data, training_data, "count")
-  save(training_data, file=file.path(expt_dir, "training_data.Rda"))
-  # 5. evaluate bias before correction
-  codon_corr_raw <- evaluate_bias(training_data, "count",
-                                  transcript_fa_fname, transcript_length_fname,
-                                  type="codon")
-  codon_corr_raw_plot <- plot_bias(codon_corr_raw)
-  save(codon_corr_raw, codon_corr_raw_plot,
-       file=file.path(expt_dir, "codon_corr_raw.Rda"))
-  nt_corr_raw <- evaluate_bias(training_data, "count",
-                               transcript_fa_fname, transcript_length_fname,
-                               type="nt")
-  nt_corr_raw_plot <- plot_bias(nt_corr_raw)
-  save(nt_corr_raw, nt_corr_raw_plot,
-       file=file.path(expt_dir, "nt_corr_raw.Rda"))
+  if(save_intermediate) {
+    save(training_data, file=file.path(expt_dir,
+                                       paste0(expt_name, "_training.Rda")))
+  }
   # 5. compute regression
+  # TODO: output warning if outlier regression values (outside -5:5 ?)
   print("Computing regression fit")
   regression_fit <- MASS::glm.nb(model, data=training_data, model=F)
   regression_coefs <- parse_coefs(regression_fit)
-  # TODO: output warning if outlier regression values (outside -5:5 ?)
-  save(regression_fit, file=file.path(expt_dir, "regression_fit.Rda"))
-  save(regression_coefs, file=file.path(expt_dir, "regression_coefs.Rda"))
   # 6. correct counts
   print("Correcting RPF counts")
-  alignment_data$corrected_count <- correct_bias(alignment_data, regression_fit)
-  training_data$corrected_count <- count_footprints(alignment_data, training_data, "corrected_count")
-  save(alignment_data, file=file.path(expt_dir, "alignment_data.Rda"))
-  save(training_data, file=file.path(expt_dir, "training_data.Rda"))
-  # 5. evaluate bias before correction
-  codon_corr_corrected <- evaluate_bias(training_data, "corrected_count",
-                                        transcript_fa_fname, transcript_length_fname,
-                                        type="codon")
-  codon_corr_corrected_plot <- plot_bias(codon_corr_corrected)
-  save(codon_corr_corrected, codon_corr_corrected_plot,
-       file=file.path(expt_dir, "codon_corr_corrected.Rda"))
-  nt_corr_corrected <- evaluate_bias(training_data, "corrected_count",
-                                     transcript_fa_fname, transcript_length_fname,
-                                     type="nt")
-  nt_corr_corrected_plot <- plot_bias(nt_corr_corrected)
-  save(nt_corr_corrected, nt_corr_corrected_plot,
-       file=file.path(expt_dir, "nt_corr_corrected.Rda"))
-  # 8. return outputs
-  output <- list(diagnostic_plot = diagnostic_plot,
-                 bam_alignment = alignment_data,
+  alignment_data$corrected <- correct_bias(alignment_data, regression_coefs)
+  training_data$corrected <- count_footprints(alignment_data, training_data,
+                                              "corrected")
+  # 7. evaluate bias before/after correction
+  print("Evaluating bias")
+  to_evaluate <- expand.grid(correction=c("count", "corrected"),
+                             type=c("codon", "nt"))
+  to_evaluate <- apply(to_evaluate, 1, paste, collapse="_")
+  positional_bias <- sapply(to_evaluate,
+                            function(x) {
+                              tmp_correction <- split(x, split="_")[[1]][1]
+                              tmp_type <- split(x, split="_")[[1]][2]
+                              tmp_bias <- evaluate_bias(training_data,
+                                                        which_column=tmp_correction,
+                                                        transcript_fa_fname,
+                                                        transcript_length_fname,
+                                                        type=tmp_type)
+                            }, simplify=F, USE.NAMES=T)
+  codon_corr_max <- max(unlist(positional_bias[grepl("codon", names(positional_bias))]))
+  nt_corr_max <- max(unlist(positional_bias[grepl("nt", names(positional_bias))]))
+  # 8. generate plots
+  codon_raw <- plot_bias(positional_bias$count_codon, type="codon") +
+    coord_cartesian(ylim=c(0, codon_corr_max))
+  codon_corrected <- plot_bias(positional_bias$corrected_codon, type="codon") +
+    coord_cartesian(ylim=c(0, codon_corr_max))
+  nt_raw <- plot_bias(positional_bias$count_nt, type="nt") +
+    coord_cartesian(ylim=c(0, nt_corr_max))
+  nt_corrected <- plot_bias(positional_bias$corrected_nt, type="nt") +
+    coord_cartesian(ylim=c(0, nt_corr_max))
+  # 9. return outputs
+  all_plots <- list(diagnostic = diagnostic_plot,
+                    codon_raw = codon_corr_raw_plot,
+                    codon_corrected = codon_corr_corrected_plot,
+                    nt_raw = nt_corr_raw_plot,
+                    nt_corrected = nt_corr_corrected_plot)
+  if(save_intermediate) {
+    save(alignment_data,
+         file=file.path(expt_dir, paste0(expt_name, "_alignments.Rda")))
+    save(training_data,
+         file=file.path(expt_dir, paste0(expt_name, "_training.Rda")))
+    save(regression_coefs,
+         file=file.path(expt_dir, paste0(expt_name, "_coefs.Rda")))
+    save(positional_bias,
+         file=file.path(expt_dir, paste0(expt_name, "_bias.Rda")))
+    save(all_plots,
+         file=file.path(expt_dir, paste0(expt_name, "_plots.Rda")))
+  }
+  output <- list(bam_alignment = alignment_data,
                  regression_data = training_data,
-                 model_fit = regression_fit,
                  regression_coefs = regression_coefs,
-                 bias = list(codon_raw = codon_corr_raw,
-                             codon_corrected = codon_corr_corrected,
-                             nt_raw = nt_corr_raw,
-                             nt_corrected = nt_corr_corrected),
-                 plots = list(codon_raw = codon_corr_raw_plot,
-                              codon_corrected = codon_corr_corrected_plot,
-                              nt_raw = nt_corr_raw_plot,
-                              nt_corrected = nt_corr_corrected_plot))
+                 plots = all_plots)
   return(output)
 }
